6b080465ac8ccf4960c3fb3964586434f28814c1
culpepperd <culpepperd2490@gmail.com>
2023-06-02 11:43:54 -0400

    added 3 additional car objects

--- objectPractice1/objectPractice1.js
+++ objectPractice1/objectPractice1.js
@@ -7,3 +7,50 @@ var taxi = {
 	convertible: false,
 	mileage: 281341
 };
+
+var cadi = {
+	make: "GM",
+	model: "Cadillac",
+	year: 1955,
+	color: "tan",
+	passengers: 5,
+	convertible: false,
+	mileage: 12892
+};
+
+var fiat = {
+	make: "Fiat",
+	model: "500",
+	year: 1957,
+	color: "Medium Blue",
+	passengers: 2,
+	convertible: false,
+	mileage: 88000
+};
+
+var chevy = {
+	make: "Chevy",
+	model: "Bel Air",
+	year: 1957,
+	color: "red",
+	passengers: 2,
+	convertible: false,
+	mileage: 1021
+};
+
+function prequal(car) {
+	if (car.mileage > 10000) {
+		return false;
+	} else if (car.year > 1960) {
+		return false;
+	}
+	return true;
+}
+
+var worthALook = prequal(taxi);
+
+if (worthALook) {
+	console.log("You gotta check out this " + taxi.make + " " + taxi.model);
+} else {
+	console.log("You should really pass on the " + taxi.make + " " + taxi.model);
+}
